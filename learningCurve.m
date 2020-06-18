5a247a6deb791366db4e4862a8af7133cb510d98
kk289 <kevilkhadka@gmail.com>
2020-06-18 03:44:44 -0700

    Add for loop

--- learningCurve.m
+++ learningCurve.m
@@ -45,22 +45,14 @@ error_val   = zeros(m, 1);
 %       for i = 1:m
 %           % Compute train/cross validation errors using training examples 
 %           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
+%           % error_train(i) and error_val(i) 
 %       end
-%
 
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
+for i = 1:m
+    theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    error_train(i) = linearRegCostFunction(X(1:i,:),y(1:i),theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+    
+end
 
 end
