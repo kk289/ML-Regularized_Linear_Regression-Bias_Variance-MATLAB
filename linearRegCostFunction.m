02bd66e4b54b16110554e2c4c427d6dc3cc63623
kk289 <kevilkhadka@gmail.com>
2020-06-18 02:10:20 -0700

    Update cost function and gradient

--- linearRegCostFunction.m
+++ linearRegCostFunction.m
@@ -19,18 +19,10 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+h = sigmoid(X * theta);
+J = (1/2*m) * sum((log(h)-y).^2) + (lambda/(2 * m)) * sum(theta(2:end).^2);
+grad = (1/m * X' * (h - y)) + [0;(lambda/m) * theta(2:end)];
 
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
 
 grad = grad(:);
 
