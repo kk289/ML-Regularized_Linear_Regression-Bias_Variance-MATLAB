97633eb01ca0e59ac5d3d6be732f6171fe94eb00
kk289 <kevilkhadka@gmail.com>
2020-06-19 08:47:07 -0700

    add validation set

--- validationCurve.m
+++ validationCurve.m
@@ -32,22 +32,14 @@ error_val = zeros(length(lambda_vec), 1);
 %           % Compute train / val errors when training linear 
 %           % regression with regularization parameter lambda
 %           % You should store the result in error_train(i)
-%           % and error_val(i)
-%           ....
-%           
+%           % and error_val(i)           
 %       end
-%
-%
 
-
-
-
-
-
-
-
-
-
-% =========================================================================
+for i = 1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    theta = trainLinearReg(X,y,lambda);
+    error_train(i) = linearRegCostFunction(X,y,theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 end
